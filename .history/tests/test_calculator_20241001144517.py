65a43314d531b711336f4e10f87c0ca2e15e0121
joshua31324324 <jo336@njit.edu>
2024-10-07 16:52:11 -0400

    Revert "idk"
    This reverts commit 4192d35535d05d836cf0bd2e1f4d63e7637b09e1.

---
+++ .history/tests/test_calculator_20241001144517.py
@@ -0,0 +1,12 @@
+'''My Calculator Test'''
+from calculator import add, subtract
+
+def test_addition():
+    '''Test that addition function works '''    
+    assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that addition function works '''    
+    assert subtract(2,2) == 0
+
+def test_multiplication(2,3) == 4   
